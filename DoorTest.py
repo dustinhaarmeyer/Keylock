659e167eed6b37ac582ae988166250c4a50d4580
dustinhaarmeyer <dustin05.dh@gmail.com>
2021-05-16 11:35:57 +0200

    Starting with the control of the Door

---
+++ DoorTest.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+pin = 37
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
+
+while True:
+    GPIO.output(pin, 1)
+    print(1)
+    sleep(1)
+    GPIO.output(pin, 0)
+    print(0)
+    sleep(1)
