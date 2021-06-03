99a9095eb83a8a3b0331bafd9430a6d84ce42f96
dustinhaarmeyer <dustin05.dh@gmail.com>
2021-06-03 10:58:28 +0200

    Useable Program
    Edit the variabel declaration of code so the Program dont start searching an RFID Tag

--- Program.py
+++ Program.py
@@ -19,7 +19,7 @@ KEYPAD = [
 ROW_PINS = [5, 6, 13, 19] # BCM numbering
 COL_PINS = [12, 16, 20] # BCM numbering
 codeLenght = 0
-code = "*"  # Delete * out of this for working version
+code = ""  # Delete * out of this for working version
 
 factory = rpi_gpio.KeypadFactory()
 keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
