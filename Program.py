581578a2077d52d161941cb727bf250a1575f8ba
dustinhaarmeyer <dustin05.dh@gmail.com>
2021-06-03 21:07:28 +0200

    Optimised Program

--- Program.py
+++ Program.py
@@ -7,9 +7,11 @@ from Database import Textfile, Textfile2
 from pad4pi import rpi_gpio
 #from LED import LED
 from Door import Door
+import flask
+from flask import redirect, request
+import multiprocessing
 
 GPIO.setwarnings(False)
-
 KEYPAD = [
     [1, 2, 3],
     [4, 5, 6],
@@ -26,10 +28,29 @@ keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PI
 r = Reader()                                                                        #RFID Reader
 d = Door(26)    #Board: 37)
 #db = Database("localhost", "securePassword", "root", "Keylock")                     #Database (mysql Server)
-userC = Textfile("/home/pi/RFID Keysave/code.txt")  #"/home/pi/Desktop/Program/code.txt")
-keyC = Textfile2("/home/pi/RFID Keysave/KeyCode.txt")  #"/home/pi/Desktop/Program/KeyCode.txt")
+userC = Textfile("/home/pi/Desktop/Program/code.txt")       #/home/pi/RFID Keysave/code.txt"
+keyC = Textfile2("/home/pi/Desktop/Program/KeyCode.txt")  #"/home/pi/RFID Keysave/KeyCode.txt"
 
-#print(db.state())
+app = flask.Flask(__name__)
+def API(conf):
+    app.run(host='0.0.0.0',port='5000')
+
+config = {"Something":"SomethingElese"}
+api = multiprocessing.Process(target=API, args=(config))
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    phoneNumber = request.args.get('phone')
+    birthdate = request.args.get('birthdate')
+
+    print(birthdate)
+    print(name)
+    print(phoneNumber)
+    print(email)
+    code = 200
+    # code = submit(name, birthdate, phoneNumber, email)
+    return "Please save this code, you need it: " + str(code)
 
 def foundKey(key):
     global code
@@ -40,6 +61,7 @@ def foundKey(key):
 
 d.close()
 print("Starting Loop!")
+api.start()
 keypad.registerKeyPressHandler(foundKey)
 while True:
     while code == "":
@@ -57,10 +79,12 @@ while True:
         print("RFID Tag: " + str(CodeNum))
         userNum = userC.search(str(CodeNum))
         print("User Number: " + str(userNum))
-        if userNum != 0: #Get User Name... to save it to who picked which key
+        if userNum != 0:
             print("User is known")
             d.open()
-            sleep(5)
+            code = ""
+            while code == "":
+                sleep(0.2)
             d.close()
         else:
             print("Not known! Please retry")
